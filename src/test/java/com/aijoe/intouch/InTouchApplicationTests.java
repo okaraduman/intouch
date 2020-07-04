0e2f36a79414f19fdab3119776d14a1b344049bb
Oguzhan Karaduman <oguzhan.kduman@gmail.com>
2020-07-04 21:21:20 +0300

    initial commit

---
+++ src/test/java/com/aijoe/intouch/InTouchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aijoe.intouch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InTouchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
