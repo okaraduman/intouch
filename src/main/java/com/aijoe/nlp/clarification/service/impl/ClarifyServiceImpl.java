4156ffc6227ee2fa6dc732a99d6d115328d6c95e
Oguzhan Karaduman <oguzhan.kduman@gmail.com>
2020-07-08 23:50:46 +0300

    - added zemberek turkish for normalization

--- src/main/java/com/aijoe/nlp/clarification/service/impl/ClarifyServiceImpl.java
+++ src/main/java/com/aijoe/nlp/clarification/service/impl/ClarifyServiceImpl.java
@@ -5,10 +5,18 @@ import MorphologicalAnalysis.FsmMorphologicalAnalyzer;
 import Ngram.NGram;
 import Ngram.NoSmoothing;
 import SpellChecker.NGramSpellChecker;
+import SpellChecker.SimpleSpellChecker;
 import com.aijoe.nlp.clarification.service.ClarifyService;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import zemberek.morphology.TurkishMorphology;
+import zemberek.normalization.TurkishSpellChecker;
+import zemberek.tokenization.Token;
+import zemberek.tokenization.TurkishTokenizer;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -17,15 +25,53 @@ public class ClarifyServiceImpl implements ClarifyService {
     @Override
     public List<String> fixSpellMistake(List<String> sentences) {
 
-        FsmMorphologicalAnalyzer fsm = new FsmMorphologicalAnalyzer();
-        NGram<String> nGram = new NGram<String>("ngram.txt");
-        nGram.calculateNGramProbabilities(new NoSmoothing<>());
-        NGramSpellChecker nGramSpellChecker = new NGramSpellChecker(fsm, nGram);
-        List<String> correctList = new ArrayList<>();
-        for(String sentence : sentences){
-            Sentence sentenceSample = new Sentence(sentence);
-            correctList.add(nGramSpellChecker.spellCheck(sentenceSample).toString());
+        TurkishMorphology morphology = TurkishMorphology.createWithDefaults();
+        List<String> newSentences = new ArrayList<>();
+        try {
+            TurkishSpellChecker spellChecker = new TurkishSpellChecker(morphology);
+            StringBuilder sb = new StringBuilder();
+            sentences.stream().forEach(sentence ->{
+                newSentences.add(sentence);
+                newSentences.add(doSth(sentence));
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return correctList;
+        return newSentences;
+    }
+
+    public String doSth(String sentence){
+        TurkishTokenizer tokenizer = TurkishTokenizer.ALL;
+        TurkishMorphology morphology = TurkishMorphology.createWithDefaults();
+        TurkishSpellChecker spellChecker = null;
+        try {
+            spellChecker = new TurkishSpellChecker(morphology);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        StringBuilder output = new StringBuilder();
+
+        for (Token token : tokenizer.tokenize(sentence)) {
+            String text = token.getText();
+            if (analyzeToken(token) && !spellChecker.check(text)) {
+                List<String> strings = spellChecker.suggestForWord(token.getText());
+                if (!strings.isEmpty()) {
+                    String suggestion = strings.get(0);
+                    output.append(suggestion);
+                } else {
+                    output.append(text);
+                }
+            } else {
+                output.append(text);
+            }
+        }
+        return output.toString();
+    }
+    static boolean analyzeToken(Token token) {
+        return token.getType() != Token.Type.NewLine
+                && token.getType() != Token.Type.SpaceTab
+                && token.getType() != Token.Type.UnknownWord
+                && token.getType() != Token.Type.RomanNumeral
+                && token.getType() != Token.Type.Unknown;
     }
 }
